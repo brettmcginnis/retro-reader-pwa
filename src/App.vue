392aa51cef4220d6b8c95d054545b898c2eece11
Brett McGinnis <brettmcgin@gmail.com>
2024-09-25 16:17:02 -0700

    Monospace reader

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import { walkthrough } from './content/ff7.ts'
+
+const asdf = walkthrough.join('\n\n')
+console.log(asdf)
 </script>
 
 <template>
@@ -11,7 +15,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="reader">
+    <HelloWorld :msg="asdf"  />
+  </div>
 </template>
 
 <style scoped>
@@ -21,10 +27,19 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.reader {
+  justify-content: start;
+  font-size: xx-small;
+  font-family: monospace;
+  /* font-size: clamp(.1rem, 1vw, 3rem); */
 }
 </style>
