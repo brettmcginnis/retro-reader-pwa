2ba99d8b361900beb9299d9836bdc7bb385ef716
Brett McGinnis <brettmcgin@gmail.com>
2025-07-07 15:18:35 -0700

    lint

--- src/components/BookmarkModal.test.tsx
+++ src/components/BookmarkModal.test.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { render, screen, waitFor, act } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { BookmarkModal } from './BookmarkModal';
 
